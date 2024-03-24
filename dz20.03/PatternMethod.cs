3015f4100286ac35f38509216721583b735880bf
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-24 12:32:10 +0200

    done

--- dz20.03/PatternMethod.cs
+++ dz20.03/PatternMethod.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using System;
+
+public class EmailSystem
+{
+    public void SendEmail(string recipient, string message)
+    {
+        Console.WriteLine($"Вiдправлено лист на адресу {recipient}: {message}");
+    }
+}
+
+public class SMSSystem
+{
+    public void SendSMS(string phoneNumber, string message)
+    {
+        Console.WriteLine($"Вiдправлено SMS на номер {phoneNumber}: {message}");
+    }
+}
+
+public class SocialMediaSystem
+{
+    public void PostToSocialMedia(string platform, string message)
+    {
+        Console.WriteLine($"Опублiковано повiдомлення на {platform}: {message}");
+    }
+}
+
+public class CommunicationFacade
+{
+    private readonly EmailSystem emailSystem;
+    private readonly SMSSystem smsSystem;
+    private readonly SocialMediaSystem socialMediaSystem;
+
+    public CommunicationFacade()
+    {
+        emailSystem = new EmailSystem();
+        smsSystem = new SMSSystem();
+        socialMediaSystem = new SocialMediaSystem();
+    }
+
+    public void SendMessageByEmail(string recipient, string message)
+    {
+        emailSystem.SendEmail(recipient, message);
+    }
+
+    public void SendMessageBySMS(string phoneNumber, string message)
+    {
+        smsSystem.SendSMS(phoneNumber, message);
+    }
+
+    public void PostMessageToSocialMedia(string platform, string message)
+    {
+        socialMediaSystem.PostToSocialMedia(platform, message);
+    }
+}
--- dz20.03/Program.cs
+++ dz20.03/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        CommunicationFacade communication = new CommunicationFacade();
+
+        string recipient = "example@example.com";
+        string phoneNumber = "+123456789";
+        string socialMediaPlatform = "Twitter";
+        string message = "Привiт, це тестове повiдомлення!";
+
+        communication.SendMessageByEmail(recipient, message);
+        communication.SendMessageBySMS(phoneNumber, message);
+        communication.PostMessageToSocialMedia(socialMediaPlatform, message);
+    }
+}
