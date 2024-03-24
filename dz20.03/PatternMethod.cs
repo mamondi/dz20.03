d3819599da0cd41df17fddd0c7b2a659677e32a8
DimamonDESKTOP <dimamon02@gmail.com>
2024-03-24 12:28:28 +0200

    done

--- dz20.03/PatternMethod.cs
+++ dz20.03/PatternMethod.cs
@@ -1,1 +1,54 @@
-﻿
+﻿public abstract class Beverage
+{
+    public abstract string GetDescription();
+    public abstract double Cost();
+}
+
+// Конкретний напій
+public class Coffee : Beverage
+{
+    public override string GetDescription()
+    {
+        return "Кава";
+    }
+
+    public override double Cost()
+    {
+        return 1.5;
+    }
+}
+
+public abstract class CondimentDecorator : Beverage
+{
+    protected Beverage beverage;
+
+    public CondimentDecorator(Beverage beverage)
+    {
+        this.beverage = beverage;
+    }
+
+    public override string GetDescription()
+    {
+        return beverage.GetDescription();
+    }
+
+    public override double Cost()
+    {
+        return beverage.Cost();
+    }
+}
+
+public class Milk : CondimentDecorator
+{
+    public Milk(Beverage beverage) : base(beverage) { }
+
+    public override string GetDescription()
+    {
+        return beverage.GetDescription() + ", Молоко";
+    }
+
+    public override double Cost()
+    {
+        return beverage.Cost() + 0.5;
+    }
+}
--- dz20.03/Program.cs
+++ dz20.03/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        Beverage coffeeWithMilk = new Milk(new Coffee());
+        Console.WriteLine($"Напiй: {coffeeWithMilk.GetDescription()}");
+        Console.WriteLine($"Цiна: ${coffeeWithMilk.Cost()}");
+    }
+}
